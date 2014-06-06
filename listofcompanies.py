bfab1883decd095461470365fe871118fd612ad0
jmontilla012 <jmontilla012@hotmail.com>
2014-06-05 20:56:37 -0400

    update

--- listofcompanies.py
+++ listofcompanies.py
@@ -6,14 +6,34 @@
 from urllib import urlopen, urlencode
 from bs4 import BeautifulSoup
 from sys import argv
+import sqlite3
+
+database = sqlite3.connect('companies.db')
+cursor = database.cursor()
 
 if len(argv) > 1:
-        for numberofpages in range(5068):
-                data = urlencode({"st":argv[1], "page": numberofpages})
-                reading = urlopen("http://www.camarasantodomingo.do/red-empresarial/busqueda?%s" % data)
-                soup = BeautifulSoup(reading)
-                for elements in soup.find_all("div", class_="__info"):
-                         item = elements.get_text()
-                         persistense = open("listofcompanies.txt", 'a')
-                         persistense.write(item.encode('utf-8'))
-                persistense.close()
+    for pages in range(int(argv[1])):
+        data = urlencode({'st':'advanced', 'page': pages})
+        output = urlopen('http://www.camarasantodomingo.do/red-empresarial/busqueda?%s' % data)
+        reading = output.read()
+        soup = BeautifulSoup(reading)
+        for elements in soup.find_all('div', class_='__info'):
+            data = elements.find_all(['h3','h4','p'])
+
+            datatostorage = ( 
+                               data[0].get_text(),
+                               data[1].get_text(),
+                               data[2].get_text(),
+                               data[3].get_text(),
+                               data[4].get_text(),
+                                
+                                 )
+            print datatostorage
+            cursor.execute('''
+                   CREATE TABLE IF NOT EXISTS Companies
+                   (nameofcompany text, place text, phone text, email text, website text)
+                         ''')
+
+            cursor.execute('INSERT INTO Companies VALUES(?,?,?,?,?)', datatostorage)
+        database.commit()
+    database.close()
